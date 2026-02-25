33515a11914e0716f621adba1e171346dd9b90e3
Developer <dev@github.com>
2026-02-26 03:19:38 +0530

    add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("hello");
+});
+app.listen(3000,(req,res)=>{
+    console.log("server on 3000");
+})
